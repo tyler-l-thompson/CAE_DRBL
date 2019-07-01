e707b97fb1f276355344a84d95f3b889c677af84
Tyler Thompson <tyler.l.thompson@wmich.edu>
2019-07-01 17:32:40 -0400

    argument parsing is solid. Improved help menu and added more options.

--- drbl-cae.sh
+++ drbl-cae.sh
@@ -1,19 +1,127 @@
 #!/usr/bin/env bash
-image_dir=/images
-mac_dir=/alva/LabInfo/mac
+image_dir="/images"
+mac_dir="/alva/LabInfo/mac"
+drbl_config_file="/etc/drbl/drblpush.conf"
 image_port_1="bond0.142:1"
 image_port_2="bond0.142:2"
 multicast_port="bond0.142"
 time_to_wait="1500"
 
-Lab_1=$1
-Lab_2=$2
-image_name=$3
-image_path=${image_dir}/${image_name}
-install_dir=$(dirname $(pwd -P))
-drbl_config_file="/etc/drbl/drblpush.conf"
-mac_path_1="${mac_dir}/${Lab_1}.txt"
-mac_path_2="${mac_dir}/${Lab_2}.txt"
+declare lab_1 lab_2 image_name mac_path_1 mac_path_2 machine_count clients_to_wait image_path
+
+function get_args () {
+    local OPTIND
+    local OPTARG
+
+    usage="
+    DRBL optimized for the CAE Center.
+    This program is capable of imaging one or two labs at the same time.
+    It is only capable of pushing an image to a lab, not pulling.
+    If you only want to image one lab, pass NONE as the second lab.
+
+    $(basename "$0") [-h|-l|-s|-w|-p] [-a LAB1] [-b LAB2] [-i IMAGE] [-t n] [-m n] [-d s] [-f s] [-o s]
+
+    where:
+        Non-Pushing Commands
+            These commands will execute and then the program will exit without setting up DRBL or Clonezilla.
+
+        -h          Show this help text.
+        -l          List images available to push.
+        -s          Stop Clonezilla and turn off imaging ports.
+        -w   LAB    Wake computers in specified lab
+        -p   int    Control imaging ports. 0 - Disable both ports. 1 - Enable one port. 2 - Enable two ports.
+
+        Pushing Commands
+            These commands will allow you to setup DRBL and Clonezilla on the server.
+
+        -a   LAB1   The name of the first lab to image. e.g. C-224
+        -b   LAB2   The name of the second lab to image. Omit if pushing to one lab.
+        -i   IMAGE  The name of the image to push.
+
+        Pushing Options
+            These commands are optional and can only be used when pushing commands are present.
+
+        -t   int    Time to wait until automatically starting the push in seconds. Default $time_to_wait seconds.
+        -m   int    Number of clients to wait to connect before starting Clonezilla. Default is all clients.
+        -d   DIR    Set the image directory path. Default: $image_dir
+        -f   DIR    Set the mac address files directory path. Default: $mac_dir
+        -o   FILE   Instead of pushing to lab, use a specified mac address file. -a and -b can be omitted.
+    "
+
+    # print help if no parameters are passed
+    if [[ -z $1 ]]; then
+        echo "$usage"
+        exit 0
+    fi
+
+    while getopts ':hlsw:p:a:b:i:t:m:f:d:o:' option; do
+      case "$option" in
+        h)
+            echo "$usage"
+            exit 0
+            ;;
+        l)
+            ls "$image_dir"
+            exit 0
+            ;;
+        s)
+            dcs -nl clonezilla-stop
+            drbl-all-service stop
+            control_ports 0
+            echo "Clonezilla has been stopped and image ports turned off."
+            exit 0
+            ;;
+        w)
+            wake_computers "${mac_dir}/${OPTARG}.txt"
+            exit 0
+            ;;
+        p)
+            control_ports "$OPTARG"
+            exit 0
+            ;;
+        a)
+            lab_1=$OPTARG
+            mac_path_1="${mac_dir}/${lab_1}.txt"
+            ;;
+        b)
+            lab_2=$OPTARG
+            mac_path_2="${mac_dir}/${lab_2}.txt"
+            ;;
+        i)
+            image_name=$OPTARG
+            ;;
+        t)
+            time_to_wait=$OPTARG
+            ;;
+        m)
+            clients_to_wait=$OPTARG
+            ;;
+        f)
+            image_dir=$OPTARG
+            ;;
+        d)
+            mac_dir=$OPTARG
+            ;;
+        o)
+            mac_path_1=$OPTARG
+            lab_1="NONE"
+            ;;
+        :)
+            printf "missing argument for -%s\n" "$OPTARG" >&2
+            echo "$usage" >&2
+            exit 1
+            ;;
+       \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+           echo "$usage" >&2
+           exit 1
+           ;;
+      esac
+    done
+    shift $((OPTIND - 1))
+
+    # set full path for image
+    image_path="${image_dir}/${image_name}"
+}
 
 function control_ports () {
     case $1 in
@@ -46,160 +154,87 @@ function wake_computers () {
 }
 
 function clonezilla_start () {
-    local machine_count=$1
-    local disk_name=$(cat "${image_path}/disk")
-    drbl-ocs -g auto -e1 auto -e2 -x -r -e -icds -j2 -a -srel -sc0 -p poweroff --clients-to-wait ${machine_count} --time-to-wait ${time_to_wait} -l en_US.UTF-8 --mcast-iface ${multicast_port} startdisk multicast_restore ${image_name} ${disk_name}
+    local disk_name=$(cat "$image_path/disk")
+    if [[ -z "$clients_to_wait" ]]; then
+        clients_to_wait=${machine_count}
+    fi
+    echo "drbl-ocs -g auto -e1 auto -e2 -x -r -e -icds -j2 -a -srel -sc0 -p poweroff --clients-to-wait ${clients_to_wait} --time-to-wait ${time_to_wait} -l en_US.UTF-8 --mcast-iface ${multicast_port} startdisk multicast_restore ${image_name} ${disk_name}"
 }
 
 function drbl_start () {
-    local machine_count=$1
-    echo "Generating config file: ${drbl_config_file}"
+    local drbl_config_header="#Setup for general
+[general]
+domain=wmich.edu
+nisdomain=morty
+localswapfile=no
+client_init=text
+login_gdm_opt=
+timed_login_time=
+maxswapsize=
+ocs_img_repo_dir=$image_path
+total_client_no=$machine_count
+create_account=
+account_passwd_length=8
+hostname=morty-
+purge_client=no
+client_autologin_passwd=
+client_root_passwd=
+client_pxelinux_passwd=
+set_client_system_select=no
+use_graphic_pxelinux_menu=yes
+set_DBN_client_audio_plugdev=no
+open_thin_client_option=no
+client_system_boot_timeout=
+language=en_US.UTF-8
+set_client_public_ip_opt=no
+config_file=drblpush.conf
+collect_mac=no
+run_drbl_ocs_live_prep=yes
+drbl_ocs_live_server=
+clonezilla_mode=clonezilla_box_mode
+live_client_branch=alternative
+live_client_cpu_mode=i386
+drbl_mode=none
+drbl_server_as_NAT_server=yes
+add_start_drbl_services_after_cfg=yes
+continue_with_one_port=
 
-    # prep the drbl config header
-    echo "#Setup for general" > ${drbl_config_file}
-    echo "[general]" >> ${drbl_config_file}
-    echo "domain=wmich.edu" >> ${drbl_config_file}
-    echo "nisdomain=morty" >> ${drbl_config_file}
-    echo "localswapfile=no" >> ${drbl_config_file}
-    echo "client_init=text" >> ${drbl_config_file}
-    echo "login_gdm_opt=" >> ${drbl_config_file}
-    echo "timed_login_time=" >> ${drbl_config_file}
-    echo "maxswapsize=" >> ${drbl_config_file}
-    echo "ocs_img_repo_dir=${image_dir}" >> ${drbl_config_file}
-    echo "total_client_no=${machine_count}" >> ${drbl_config_file}
-    echo "create_account=" >> ${drbl_config_file}
-    echo "account_passwd_length=8" >> ${drbl_config_file}
-    echo "hostname=morty-" >> ${drbl_config_file}
-    echo "purge_client=no" >> ${drbl_config_file}
-    echo "client_autologin_passwd=" >> ${drbl_config_file}
-    echo "client_root_passwd=" >> ${drbl_config_file}
-    echo "client_pxelinux_passwd=" >> ${drbl_config_file}
-    echo "set_client_system_select=no" >> ${drbl_config_file}
-    echo "use_graphic_pxelinux_menu=yes" >> ${drbl_config_file}
-    echo "set_DBN_client_audio_plugdev=no" >> ${drbl_config_file}
-    echo "open_thin_client_option=no" >> ${drbl_config_file}
-    echo "client_system_boot_timeout=" >> ${drbl_config_file}
-    echo "language=en_US.UTF-8" >> ${drbl_config_file}
-    echo "set_client_public_ip_opt=no" >> ${drbl_config_file}
-    echo "config_file=drblpush.conf" >> ${drbl_config_file}
-    echo "collect_mac=no" >> ${drbl_config_file}
-    echo "run_drbl_ocs_live_prep=yes" >> ${drbl_config_file}
-    echo "drbl_ocs_live_server=" >> ${drbl_config_file}
-    echo "clonezilla_mode=clonezilla_box_mode" >> ${drbl_config_file}
-    echo "live_client_branch=alternative" >> ${drbl_config_file}
-    echo "live_client_cpu_mode=i386" >> ${drbl_config_file}
-    echo "drbl_mode=none" >> ${drbl_config_file}
-    echo "drbl_server_as_NAT_server=yes" >> ${drbl_config_file}
-    echo "add_start_drbl_services_after_cfg=yes" >> ${drbl_config_file}
-    echo "continue_with_one_port=" >> ${drbl_config_file}
+#Setup for $image_port_1
+[$image_port_1]
+interface=$image_port_1
+mac=$mac_path_1
+ip_start=1
+"
 
-    # prep the drbl config interfaces
-    echo "" >> ${drbl_config_file}
-    echo "#Setup for ${image_port_1}" >> ${drbl_config_file}
-    echo "[${image_port_1}]" >> ${drbl_config_file}
-    echo "interface=${image_port_1}" >> ${drbl_config_file}
-    echo "mac=${mac_path_1}" >> ${drbl_config_file}
-    echo "ip_start=1" >> ${drbl_config_file}
-    echo ""  >> ${drbl_config_file}
+    local drbl_config_second_interface="#Setup for $image_port_2
+[$image_port_2]
+interface=$image_port_2
+mac=$mac_path_2
+ip_start=1
+"
 
-    if [[ ${Lab_2} != "NONE" ]]
-    then
-        echo "#Setup for ${image_port_2}" >> ${drbl_config_file}
-        echo "[${image_port_2}]" >> ${drbl_config_file}
-        echo "interface=${image_port_2}" >> ${drbl_config_file}
-        echo "mac=${mac_path_2}" >> ${drbl_config_file}
-        echo "ip_start=1" >> ${drbl_config_file}
-        echo ""  >> ${drbl_config_file}
+    echo "Generating config file: $drbl_config_file"
+    echo "$drbl_config_header" > "$drbl_config_file"
+
+    if [[ ! -z ${lab_2} ]]; then
+        echo "$drbl_config_second_interface" >> "$drbl_config_file"
     fi
 
-    drblpush -c ${drbl_config_file}
+    # drblpush -c ${drbl_config_file}
 }
 
 function user_confirmation () {
-    echo "$1 [ yes | no ]"
+    echo "$1 [y|n]"
     read user_input
-    if [[ "$user_input" == "no" ]]
-    then
+    if [[ "$user_input" != "y" ]]; then
         echo "I'll stop here then."
         exit 0
     fi
 }
 
-function read_file_lines () {
-    while read line
-    do
-        echo " ${line}"
-    done < "$1"
-}
-
-function get_args () {
-    local OPTIND
-    local OPTARG
-
-    usage="
-    DRBL optimized for the CAE Center.
-    This program is capable of imaging one or two labs at the same time.
-    It is only capable of pushing an image to a lab, not pulling.
-    If you only want to image one lab, pass NONE as the second lab.
-
-    $(basename "$0") [-h] [-l] [-s] [-t n] LAB1 LAB2 IMAGE
-
-    where:
-        -h          Show this help text.
-        -l          List images available to push.
-        -s          Stop Clonezilla and turn off imaging ports.
-        -t    int   Time to wait until automatically starting the push in seconds. Default 1200 seconds.
-        LAB1        The name of the first lab to image. e.g. C-224
-        LAB2        The name of the second lab to image. Pass NONE for only one lab. e.g. C-226
-        IMAGE       The name of the image to push.
-    "
-
-    # print help if no parameters are passed
-    if [[ -z $1 ]]
-    then
-        echo "$usage"
-        exit 0
-    fi
-
-    while getopts ':hlst:' option; do
-      case "$option" in
-        h)
-            echo "$usage"
-            exit 0
-            ;;
-        l)
-            ls "$image_dir"
-            exit 0
-            ;;
-        s)
-            dcs -nl clonezilla-stop
-            drbl-all-service stop
-            control_ports 0
-            echo "Clonezilla has been stopped and image ports turned off."
-            exit 0
-            ;;
-        t)
-            time_to_wait=$OPTARG
-            ;;
-        :)
-            printf "missing argument for -%s\n" "$OPTARG" >&2
-            echo "$usage" >&2
-            exit 1
-            ;;
-       \?) printf "illegal option: -%s\n" "$OPTARG" >&2
-           echo "$usage" >&2
-           exit 1
-           ;;
-      esac
-    done
-    shift $((OPTIND - 1))
-
-}
-
 function check_user () {
     # make sure we are root
-    if [[ "$USER" != "$1" ]]
-    then
+    if [[ "$USER" != "$1" ]]; then
         echo "This script must be run as $1."
         exit 0
     fi
@@ -208,8 +243,7 @@ function check_user () {
 function check_lab_name () {
     lab_mac_file="$mac_dir"/"$1".txt
     echo "Checking if $lab_mac_file exists..."
-    if [[ -f "$lab_mac_file" ]]
-    then
+    if [[ -f "$lab_mac_file" ]]; then
         echo "File found."
     else
         echo "Lab mac file not found: $lab_mac_file"
@@ -218,65 +252,68 @@ function check_lab_name () {
 }
 
 function main () {
+    # check user is root
+    check_user "root"
+
+    # get command line arguments
+    get_args "$@"
 
     # check if labs are the same
-    if [[ ${Lab_1} == ${Lab_2} ]]
-    then
+    if [[ ${lab_1} == ${lab_2} ]]; then
         echo "You must pass two different labs as parameters."
         exit 1
     fi
 
     # check if lab mac files exits
-    check_lab_name ${Lab_1}
-    if [[ ${Lab_2} != "NONE" ]]
-    then
-        check_lab_name ${Lab_2}
-    fi
+    if [[ "$lab_1" != "NONE" ]]; then
+        check_lab_name ${lab_1}
+        if [[ ! -z ${lab_2} ]]; then
+            check_lab_name ${lab_2}
+        fi
+     fi
 
     # check if image directory exits
-    if [[ -d ${image_path} ]]
-    then
-        echo "Image path found."
+    if [[ -z ${image_name} ]]; then
+        echo "Image name must be specified with -i. See -h help menu."
+        exit 1
+    fi
+    if [[ -d ${image_path} ]]; then
+        echo "Image path $image_path found."
     else
-        echo "Image path not found. Double check spelling of image name."
+        echo "Image path $image_path not found. Double check spelling of image name."
         exit 1
     fi
 
-    # enable image ports
-    if [[ ${Lab_2} == "NONE" ]]
-    then
-        control_ports 1
-    else
-        control_ports 2
-    fi
-
     # calc number of computers
-    if [[ ${Lab_2} == "NONE" ]]
-    then
-        machine_count=$(wc -l < ${mac_path_1})
-    else
-        machine_count=$(($(wc -l < ${mac_path_1}) + $(wc -l < ${mac_path_2})))
+    machine_count=$(wc -l < ${mac_path_1})
+    if [[ ! -z ${lab_2} ]]; then
+        machine_count=$((${machine_count} + $(wc -l < ${mac_path_2})))
     fi
+    echo "Total machines to image: $machine_count"
+
+    # enable image ports
+#    user_confirmation "Ready to enable imaging ports?"
+#    if [[ ! -z ${lab_2} ]]; then
+#        control_ports 2
+#    else
+#        control_ports 1
+#    fi
 
     user_confirmation "Ready to setup DRBL?"
-    drbl_start ${machine_count}
+    drbl_start
 
     user_confirmation "Ready to start Clonezilla?"
-    clonezilla_start ${machine_count}
+    clonezilla_start
 
-    user_confirmation "Ready to wakeup computers in ${Lab_1}?"
+    user_confirmation "Ready to wakeup computers in ${lab_1}?"
     wake_computers ${mac_path_1}
 
-    if [[ ${Lab_2} != "NONE" ]]
-    then
-        user_confirmation "Ready to wakeup computers in ${Lab_2}?"
+    if [[ ! -z ${lab_2} ]]; then
+        user_confirmation "Ready to wakeup computers in ${lab_2}?"
         wake_computers ${mac_path_2}
     fi
 
 }
 
-check_user "root"
-get_args $*
-echo "$Lab_1 $Lab_2 $image_name"
-#main
+main "$@"
 exit 0
